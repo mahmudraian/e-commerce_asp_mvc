297aed26465c71af5eaf59743b3a911eb3f669e1
mahmudraian <raian.osthir@gmail.com>
2024-10-23 02:16:20 +0600

    Add project files.

---
+++ Models/Product.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+
+namespace e_commerce.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public string ProductDescription { get; set; }
+
+        public int ProductCategoryId { get; set; }
+        
+        public  int review_id { get; set; }       
+
+
+        public int ProductCount { get; set; }
+
+        public int ProductQuantity { get; set; }
+
+        public DateTime created_at { get; set; }
+
+        public DateTime updated_at { get; set;}
+
+
+
+    }
+}
